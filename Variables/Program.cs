03babad5ebda74bd208dc7b6407c431c97e374c7
KangDoYoung <popioiking@naver.com>
2022-06-15 21:55:54 +0900

    method , 객체 클래스 인스턴트
    추가+

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,11 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*// see https://aka.ms/new-console-template for more information
 
-using System;
+using system;
 
 int cunt;
 // 전역번수 
 
-namespace Variable
+namespace variable
 {
 
 
@@ -13,10 +13,10 @@ namespace Variable
     class program
     {
      
-            static void Main(string[] args)
+            static void main(string[] args)
             {
-                Console.WriteLine(".");
-                Console.WriteLine(Human.age);
+                console.writeline(".");
+                console.writeline(human.age);
             }
        
     }
@@ -24,21 +24,21 @@ namespace Variable
 
 
 
-class Human
+class human
 {
 
     // bit = 한자리 이진수.( 0과 1 . 정보 처리의 최소단위 ) 
-    // 1 byte = 8 bit. ( CPU의 데이터 처리 최소 단위. ) 
+    // 1 byte = 8 bit. ( cpu의 데이터 처리 최소 단위. ) 
     // 4 byte = 8*4 bit = 32 bit. 
     // 표현할 수 있는 수의 범위는 2^(bit 수). 
     public static int age = 1; // 4 byte 정수 -2147483648~2147483647 
     public float height; // 4 byte 실수 
-    public bool isResting; // 1 byte 논리 
-    public char genderChar; // 2 byte 문자 
+    public bool isresting; // 1 byte 논리 
+    public char genderchar; // 2 byte 문자 
     public string name; // 문자열. 문자 갯수 * 2 byte 크기 
 
     // 멤버 : 클래스, 구조ㅊㅔ, 네임스페이스 구성하는 멤버변수
 
-}
+} */
 
 
---
+++ method/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+// 함수: 특정 연산에 대한 기능
+//method :  함수 중에서 객체를 통해서 호출되는 함수 
+namespace methods
+{
+    internal class program
+    {
+
+        static int b = 3;
+        //program 클래스의 멤버 함수
+        static void Main(string[] args)
+        {
+            PrintHelloWorld();
+            PrintNum(3); // 3
+            PrintSumResult(1, 2); //1 + 2 = 3
+
+
+            //지역변수 : 함수안에서 선언된 변수
+            //지역변수가 할당되는 공간은 어떤 값이 들어있는지 알 수 없으므로
+            //초기화를 해주어야한다
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(Sum(a, b)); // 5
+
+
+            float a1 = 5.25f;
+            float b2 = 4.24f;
+
+            Console.WriteLine(Son(a1, b2));
+
+            Console.WriteLine(5); // 5
+            
+        }
+
+        //함수의 형태
+        //매개변수 자료형 매개변수 이름
+        // 반환 : 제어권을 넘겨준다
+        // void 반환시에 특별한 값을 넘기지않는다
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static void PrintNum(int num)
+        {
+            Console.WriteLine(num);
+        }
+
+
+        static void PrintSumResult(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static int Sum (int a, int b)
+        {
+            return a + b;
+        }
+        
+        static float Son(float a1, float b2)
+        {
+            return a1 - b2;
+        }
+    }
+}
