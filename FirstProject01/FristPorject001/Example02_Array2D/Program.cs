c37564e33d5ac97702add1821e8011796f87815c
Dohyeon00 <popioiking@naver.com>
2022-06-23 21:55:32 +0900

    배열 반목문 플레이어 이동게임

---
+++ FirstProject01/FristPorject001/Example02_Array2D/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace Example02_
+{
+    internal class Program
+    {
+
+        static int[,] map = new int[5, 5]
+        {
+            {0, 0, 0 ,0, 1 },
+            {0, 1, 1, 1, 1 },
+            {0, 0, 0, 1, 1 },
+            {1, 1, 0, 0, 0 },
+            {0, 1, 1, 0, 0 },
+        };
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.x = 3;
+            player.y = 0;
+            player.MoveLeft(map);
+        }
+    }
+
+    class Player
+    {
+        public int x, y;
+
+        public void MoveLeft(int[,] map)
+        {
+            if (x - 1 < 0)
+            {
+                Console.WriteLine($"플레이어를 왼쪽으로 이동시킬 수 없습니다. (맵의 경계)");
+            }
+            else if (map[y, x - 1] == 0)
+            {
+                x--;
+                Console.WriteLine($"플레이어가 왼쪽으로 한칸이동, 현재위치 {x}, {y}");
+            }
+            else if (map[y, x - 1] == 1)
+            {
+                Console.WriteLine($"플레이어를 왼쪽으로 이동시킬 수 없습니다. (벽)")
+                
+            }
+        }
+    }
+}
---
+++ FirstProject01/FristPorject001/Statement_Loops/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Statement_Loops
+{
+
+    // 반복문의 종류
+    // while, do while,for, forcach
+    // while 조건이 참이면 해당 내용 반복수행
+    // do while 일반 한번 수행하고 조건이 참이면 해당내용 반복 수행
+    // for 조건이 참이면 해당내용 반복수행하며 반복수행이 끝날때 마다 특정 연산이 가능
+    // foreach cillection 또는 배열의 모든 요소에 접근해서 반복수행
+
+
+
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //while (true)
+            //{
+            //    Console.WriteLine(1);
+
+
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(1);
+            //} while (true);
+
+
+            int[] tmpArr = new int[5];
+            
+
+            for (int i = 0; i < tmpArr.Length; i++)
+            {
+                Console.WriteLine(tmpArr[i]);
+            }
+
+
+            // 다차원 배열
+            int[,] mat2D = new int[3, 5]
+            {
+                { 1, 2, 3, 4, 5},
+                { 4, 5, 6, 7, 8},
+                { 4, 5, 6, 0, 4},
+
+                
+            };
+
+            for (int i = 0; i< mat2D.GetLongLength(0); i++)
+            {
+                for (int j = 0; j < mat2D.GetLength(1); j++)
+                {
+                    Console.Write(mat2D[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+
+            //// collection 내의 모든 요소들에 접근
+            //// 접근시 해당요소는 var item 지역변수에 대입
+            //foreach (var item in collection)
+            //{
+            //}
+
+            int[][] arr2D = new int[5][];
+            for (int i = 0; i < arr2D.Length; i++)
+            {
+                arr2D[i] = new int[3];
+            }
+        
+
+            foreach (int item in tmpArr)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+
+}
