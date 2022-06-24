f5fc9871eb4db08490f6d89d517d952678a9021e
Do <popioiking@naver.com>
2022-06-24 21:54:52 +0900

    C# - 2DArrayExample DynamicArray
    ++

---
+++ FirstProject01/Example03_DynamicAarry/DynamicArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03_DynamicArray
+{
+    internal class DynamicArray
+    {
+
+
+        
+        const int DEFAULT_SIZE = 1; // const 키워드 : 해당 변수를 상수형태로 사용하겠다는 키워드
+        int a = DEFAULT_SIZE;
+        int[] _data = new int[DEFAULT_SIZE];
+
+        public int Lenght;
+        public int Capacify;
+
+        public void Add(int item)
+        {
+
+           if(Lenght >= Capacify)
+           {
+                int[] tmp = new int[Capacify * 2];
+
+                for (int i = 0; i < Lenght; i++)
+                {
+                    tmp[i] = _data[i];
+                }
+                _data = tmp;
+           }
+           _data[Lenght] = item;
+           Lenght++;
+        }
+
+        
+
+    }
+}
